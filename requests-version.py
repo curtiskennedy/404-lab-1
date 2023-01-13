3cb69bf1764bf0828a210ecf51c806af326a5fec
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-01-13 16:13:17 -0700

    script downloads and prints itself

--- requests-version.py
+++ requests-version.py
@@ -9,3 +9,10 @@ res = requests.get("http://www.google.com/")
 
 # print res object
 print("Google Homepage:", res)
+
+# get the source code of this script from github
+raw = "https://raw.githubusercontent.com/curtiskennedy/404-lab-1/main/requests-version.py"
+res = requests.get(raw)
+
+# print the source code
+print("\nSCRIPT SOURCE CODE:\n", res.text)
